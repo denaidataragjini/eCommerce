15df45d474f7deb65bf099776350519aee84b753
Rina Bedini <rina@e-soft.al>
2024-04-22 16:43:04 +0200

    added category and brand pages

--- ecommerce/app/urls.py
+++ ecommerce/app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.home),
+    path('category/',views.CategoryView.as_view(),name="category"),
+    path('brand/',views.BrandView.as_view(),name="brand"),
 ]
--- ecommerce/app/views.py
+++ ecommerce/app/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
 def home(request):
     return render(request,"app/home.html")
+
+# There are two ways to render the page, by class and by function
+class CategoryView(View):
+    def get(self,request):
+        return render(request,"app/category.html")
+
+class BrandView(View):
+    def get(self,request):
+        return render(request,"app/brand.html")
